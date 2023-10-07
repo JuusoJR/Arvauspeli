a3dda037b54fef7661a9a1968d0af502b794d1cf
JuusoJR <juuso.rantonen@gmail.com>
2023-10-07 19:45:08 +0300

    Create Guess.java

---
+++ Guess.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Guess {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String correctName = "Matti";
+        String guess = "";
+
+        do {
+            System.out.println("Arvaa nimeni");
+            guess = in.nextLine();
+
+            if (guess.equalsIgnoreCase(correctName)) {
+
+                System.out.println("Onneksi olkoon! Arvasit oikein");
+
+            } else {
+                System.out.println("Yritä uudestaan!");
+            }
+        } while (!guess.equalsIgnoreCase(correctName));
+
+    }
+
+}
